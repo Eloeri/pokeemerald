c68cb552f9351f771ed18224c8e288a8041d23bd
Eloeri <majorasdeitymask@gmail.com>
2025-11-01 18:33:07 -0500

    Default running speed

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -651,20 +651,16 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
         return;
     }
 
-    if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER) && (heldKeys & A_BUTTON) | (heldKeys & B_BUTTON) && FlagGet(FLAG_SYS_B_DASH)
+    if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER) && (heldKeys & B_BUTTON) && FlagGet(FLAG_SYS_B_DASH)
      && IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior) == 0)
     {
-        PlayerRun(direction);
-        gPlayerAvatar.flags |= PLAYER_AVATAR_FLAG_DASH;
+        PlayerWalkNormal(direction);
         return;
     }
     else
     {
-        //PlayerWalkNormal(direction);
-		if ((heldKeys & A_BUTTON) | (heldKeys & B_BUTTON))
-			PlayerRun(direction);
-		else
-			PlayerWalkNormal(direction);
+        PlayerRun(direction);
+        gPlayerAvatar.flags |= PLAYER_AVATAR_FLAG_DASH;
 	}
 }
 
